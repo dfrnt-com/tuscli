704979d829af27d14561b5f92ce2dae1f495a797
Philippe Höij <philippe@hoij.net>
2022-01-10 04:26:04 +0100

    Updated to 0.0.9 with build

--- src/tuscli.ts
+++ src/tuscli.ts
@@ -8,7 +8,7 @@ import fs from "fs";
 const program = new Command();
 
 program
-  .version("0.0.6")
+  .version("0.0.9")
   .description("TerminusDB Javascript cli: tuscli [options] <fileName(s)>")
   .option("-c, --create", "create document from provided file")
   .option("-r, --read <document-id>", "read document-id (Type/id)")
